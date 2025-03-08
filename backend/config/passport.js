17298e42b6029c2d4c7e1e76c5766fe692df91c4
sonu kumar <snu0929@gmail.com>
2025-03-08 19:34:57 +0530

    Updated Google OAuth callback URL again

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -8,7 +8,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
-      // callbackURL: "/auth/google/callback",
+      callbackURL: "https://recipe-assignm.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
